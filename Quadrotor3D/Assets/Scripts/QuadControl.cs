7565446b6023870a6d85f7a5f4bad0f732a4e43e
BhavyanshM <bhavyanshmishra@gmail.com>
2020-02-07 00:30:23 -0600

    Add the quadrotor unity project

---
+++ Quadrotor3D/Assets/Scripts/QuadControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadControl : MonoBehaviour
+{
+	public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	Physics.gravity = new Vector3(0, -9.8f, 0);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,9.8f,0, ForceMode.Acceleration);
+        
+    }
+}
